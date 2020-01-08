d8acf48a94018a62f24125edd0d8b4e34cba3a71
Allen Ellis <diamondstateellis@gmail.com>
2020-01-07 22:24:15 -0600

    Introduction to functions

---
+++ Lesson6.js
@@ -0,0 +1,17 @@
+// Introduction to functions
+// Exercise One:
+// Create a function named 'myFirstFunction', in it console log "this is my first function!"
+function myFirstFunction(){
+  console.log("this is my first function!");
+}
+
+// myFirstFunction(); // <-- use this to call function
+
+
+// Exercise Two:
+// Create a function named 'mySecondFunction' in it console log 'this is my second function!'
+function mySecondFunction(){
+  console.log("this is my second function!");
+}
+
+// mySecondFunction(); // <-- use this to call function
